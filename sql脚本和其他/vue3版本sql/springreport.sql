cfe5f3bfa97c7f3e9b11a95889532e0769905364
caiyang <caiyang90@163.com>
2024-08-12 21:12:02 +0800

    脚本更新

--- sql脚本和其他/vue3版本sql/springreport.sql
+++ sql脚本和其他/vue3版本sql/springreport.sql
@@ -1738,3 +1738,25 @@ INSERT INTO `screen_tpl` VALUES (1819184374521495554, 'SR00000000', 'tpl002', 't
 
 update sys_menu set del_flag = 1 WHERE id = 1421971956013498370
 -- 20240805 大屏功能 end
+-- 20240812 word新增图表支持 start
+DROP TABLE IF EXISTS `doc_tpl_charts`;
+CREATE TABLE `doc_tpl_charts`  (
+  `id` bigint(20) NOT NULL COMMENT '主键id',
+  `tpl_id` bigint(20) NULL DEFAULT NULL COMMENT '模板id',
+  `chart_url` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '图表对应的图片链接',
+  `chart_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '图表名称',
+  `show_chart_name` tinyint(4) NULL DEFAULT 1 COMMENT '图表是否显示名称 1是 2否',
+  `dataset_id` bigint(20) NULL DEFAULT NULL COMMENT '数据集id',
+  `dataset_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '数据集名称',
+  `category_field` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '分组字段',
+  `value_field` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '数值字段',
+  `series_field` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '系列字段',
+  `chart_type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '图表类型',
+  `creator` bigint(20) NULL DEFAULT NULL COMMENT '创建人',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `updater` bigint(20) NULL DEFAULT NULL COMMENT '更新人',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `del_flag` tinyint(4) NULL DEFAULT 1 COMMENT '删除标记 1未删除 2已删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+-- 20240812 word新增图表支持 end
