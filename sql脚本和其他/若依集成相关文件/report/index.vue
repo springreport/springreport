02f2f17f463bad1f911d73a259155a761726224f
caiyang <caiyang90@163.com>
2025-01-10 20:53:22 +0800

    新增与若依框架集成相关文件

---
+++ sql脚本和其他/若依集成相关文件/report/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <iframe :src="getLinkUrl" style="width: 100%; height: calc(100vh - 87px); border: none;" />
+  </div>
+</template>
+
+<script>
+import { getToken } from '@/utils/auth'
+export default {
+  name: 'reportLink',
+  computed: {
+    getLinkUrl() {
+      const tempUrl = this.$route.query ? this.$route.query.url : undefined
+      if (tempUrl != null) {
+        if (tempUrl.indexOf('?') === -1) {
+          return tempUrl + '?thirdPartyType=ruoyi&token=' + getToken()
+        } else {
+          return tempUrl + '&thirdPartyType=ruoyi&token=' + getToken()
+        }
+      }
+      return tempUrl
+    }
+  }
+}
+</script>
+
+<style>
+</style>
