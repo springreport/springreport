5dae539157d420bca44cc2363b2ec355c1368fff
qi15153947520 <52560840+qi15153947520@users.noreply.github.com>
2025-01-12 03:28:39 +0800

    底部备案信息宽度更改

--- SpringReport-ui-vue3/src/layouts/components/MainFooter/index.vue
+++ SpringReport-ui-vue3/src/layouts/components/MainFooter/index.vue
@@ -8,11 +8,12 @@
 -->
 <template>
   <el-footer class="footer df-c">
-    <div class="filing-information df-c">
+    <div class="filing-information df-c-c">
       <a
         target="_blank"
         href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=37021302001006"
-      >鲁公网安备 37021302001006号</a>
+        >鲁公网安备 37021302001006号</a
+      >
       <img src="@/assets/img/aicon.png" class="beian-icon" />
       <a target="_blank" href="https://beian.miit.gov.cn/">鲁ICP备2021031250号-2</a>
     </div>
@@ -20,24 +21,24 @@
   </el-footer>
 </template>
 <style lang="scss" scoped>
-.footer {
-  height: 44px !important;
-}
-.filing-information {
-  text-align: center;
-  width: fit-content;
-  margin: 0 auto;
-  a {
-    color: rgba(0, 0, 0, 0.4);
-    font-size: 12px;
-    line-height: 16px;
-    height: 16px;
-    text-decoration: none;
+  .footer {
+    height: 44px !important;
   }
-  .beian-icon {
-    width: 16px;
-    height: 16px;
-    margin-left: 8px;
+  .filing-information {
+    text-align: center;
+    width: fit-content;
+    margin: 0 auto;
+    a {
+      color: rgba(0, 0, 0, 0.4);
+      font-size: 12px;
+      line-height: 16px;
+      height: 16px;
+      text-decoration: none;
+    }
+    .beian-icon {
+      width: 16px;
+      height: 16px;
+      margin-left: 8px;
+    }
   }
-}
 </style>
