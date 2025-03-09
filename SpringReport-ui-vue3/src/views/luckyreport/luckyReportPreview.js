66dd294e1b8c0dcb10facda843aed9c2135c6650
caiyang <caiyang90@163.com>
2025-03-09 09:41:27 +0800

    bug修复

--- SpringReport-ui-vue3/src/views/luckyreport/luckyReportPreview.js
+++ SpringReport-ui-vue3/src/views/luckyreport/luckyReportPreview.js
@@ -1501,6 +1501,9 @@ export default {
       if(this.tplType == 2){
         if (this.cellAllowEditConfigs[sheetIndex]) {
           var orginCell = this.extendCellOrigins[sheetIndex][key]
+          if (!orginCell) {
+            orginCell = this.getNewCellExtendOrigins(sheetIndex, r, c)
+          }
           if (orginCell) {
             var allowEdit = this.cellAllowEditConfigs[sheetIndex][orginCell.r + '_' + orginCell.c]
             if (!allowEdit) {
@@ -2209,7 +2212,7 @@ export default {
       }
     },
     //获取新增单元格对应的原始数据
-    //获取x坐标相邻的单元格，最多获取5个相邻的单元格，如果获取不到那就忽略该单元格
+    //获取x坐标相邻的单元格，最多获取2个相邻的单元格，如果获取不到那就忽略该单元格
     getNewCellExtendOrigins(sheetIndex, r, c) {
       let originCell = null;
       if (
@@ -2219,7 +2222,7 @@ export default {
         originCell = this.addExtendCellOrigins[sheetIndex][r + '_' + c];
         return originCell;
       }
-      for (let index = 1; index <= 5; index++) {
+      for (let index = 1; index <= 2; index++) {
         let prevr = r - index;
         if (prevr < 0) {
           break;
@@ -2251,7 +2254,7 @@ export default {
           }
         }
       }
-      for (let index = 1; index <= 5; index++) {
+      for (let index = 1; index <= 2; index++) {
         let prevc = c - index;
         if (prevc < 0) {
           break;
