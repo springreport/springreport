fcdd67ca37c5b7e930e9a7398bb3acb986f36568
caiyang <caiyang90@163.com>
2025-10-23 15:30:40 +0800

    #ID2SHX和#ID2EUK两个issue修复

--- SpringReport-ui-vue3/src/views/luckyreport/luckyReportPreview.js
+++ SpringReport-ui-vue3/src/views/luckyreport/luckyReportPreview.js
@@ -1571,7 +1571,7 @@ export default {
           originCell = this.getNewCellExtendOrigins(sheetIndex, r, c);
         }
         if (originCell) {
-          var v = newValue ? newValue.m : '';
+          var v = newValue ? (newValue.m? newValue.m:(newValue.v+'')): ''
           if (v) {
             v = v.trim();
           }
@@ -1732,22 +1732,31 @@ export default {
       }
     },
     removeErrorSetting(r, c, order) {
-      var key = r + '_' + c;
-      var sheetIndex = luckysheet.getSheet().index;
-      var orginCell = this.extendCellOrigins[sheetIndex][key];
+      var key = r + '_' + c
+      let luckysheetFile = luckysheet.getSheet();
+      var sheetIndex = luckysheetFile.index
+      let data = luckysheetFile.data;
+      var orginCell = this.extendCellOrigins[sheetIndex][key]
       if (!orginCell) {
-        orginCell = this.getNewCellExtendOrigins(sheetIndex, r, c);
+        orginCell = this.getNewCellExtendOrigins(sheetIndex, r, c)
       }
       if (orginCell.ps) {
-        luckysheet.setCellFormat(r, c, 'ps', null, order);
-        luckysheet.setCellFormat(r, c, 'ps', orginCell.ps, order);
+        if(data[r] && data[r][c]){
+            data[r][c].ps = orginCell.ps;
+        }
       } else {
-        luckysheet.setCellFormat(r, c, 'ps', null, order);
+        if(data[r] && data[r][c]){
+          data[r][c].ps = null;
+        }
       }
       if (orginCell.bg) {
-        luckysheet.setCellFormat(r, c, 'bg', orginCell.bg, order);
+        if(data[r] && data[r][c]){
+          data[r][c].bg = orginCell.bg
+        }
       } else {
-        luckysheet.setCellFormat(r, c, 'bg', null, order);
+        if(data[r] && data[r][c]){
+          data[r][c].bg = null
+        }
       }
     },
     closeEditDialog() {
@@ -1765,7 +1774,7 @@ export default {
       this.$refs['editFormRef'].validate((valid) => {
         if (valid) {
           luckysheet.setCellValue(this.editR, this.editC, this.editForm.cellContent + '', {
-            isRefresh: false,
+            isRefresh: true,
           });
           luckysheet.setRangeShow({
             row: [this.editR, this.editR],
